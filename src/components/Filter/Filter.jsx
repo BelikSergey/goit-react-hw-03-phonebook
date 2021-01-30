13112235c06f88a2b89400a839909d200f246d13
BelikSergey <65306747+BelikSergey@users.noreply.github.com>
2021-01-30 13:13:08 +0200

    Добавил хранение, добавление и даление в локалсторейдж

---
+++ src/components/Filter/Filter.jsx
@@ -0,0 +1,13 @@
+const Filter = ({ filter, onChange }) => {
+  // console.log('click');
+  return (
+    <input
+      type="text"
+      name="filter"
+      value={filter}
+      placeholder="Search by contacts"
+      onChange={({ target }) => onChange(target.value)}
+    />
+  );
+};
+export default Filter;
